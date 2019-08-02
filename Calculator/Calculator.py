550e12e9ebd532599c8e97bca9e98e5107ad7086
Rahul <vaish.rahul4810@gmail.com>
2019-08-02 22:37:43 +0530

    Calculator

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -9,34 +9,31 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Addition(Resource):
-    def get(self, first_number, second_number):
-        return {'data': sumTwoNumbers.sumTwo(first_number,second_number)}
 
-		
-class Substraction(Resource):
-    def get(self, first_number, second_number):
-        return {'data': substractTwoNumbers.substractTwo(first_number,second_number)}
+@app.route('/welcome')
+def api_welcome():
+    return 'Welcome to Simple calculator Flask Application'
 
-
-
-class Multiplication(Resource):
-    def get(self, first_number, second_number):
-        return {'data': multiplyTwoNumbers.multiplyTwo(first_number,second_number)}
-
-
-
-class Division(Resource):
-    def get(self, first_number, second_number):
-        return {'data': divideTwoNumbers.divideTwo(first_number,second_number)}
-
-
-
-		
-api.add_resource(Addition, '/addition/<first_number>/<second_number>')
-api.add_resource(Substraction, '/substraction/<first_number>/<second_number>')
-api.add_resource(Multiplication, '/multiplication/<first_number>/<second_number>')
-api.add_resource(Division, '/division/<first_number>/<second_number>')
-
+@app.route('/addition', methods = ['POST'])
+def api_addition():
+    content = request.get_json()
+    return str(sumTwoNumbers.sumTwo(content['firstnumber'],content['secondnumber']))
+            
+@app.route('/multiplication', methods = ['POST'])
+def api_multiplication():
+    content = request.get_json()  
+    return str(multiplyTwoNumbers.multiplyTwo(content['firstnumber'],content['secondnumber']))
+            
+@app.route('/division', methods = ['POST'])
+def api_division():
+    content = request.get_json()
+    return str(divideTwoNumbers.divideTwo(content['firstnumber'],content['secondnumber']))
+            
+@app.route('/substraction', methods = ['POST'])
+def api_substraction():
+    content = request.get_json()
+    return str(substractTwoNumbers.substractTwo(content['firstnumber'],content['secondnumber']))
+            
+    
 if __name__ == '__main__':
      app.run(debug=True,host='0.0.0.0')
